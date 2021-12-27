dd0eb03888c429e2924d6f193e10bee1b09f3559
Joel de Oliveira <coffee.program.joel@gmail.com>
2021-12-27 09:19:00 -0300

    Modificações iniciais

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App subject="Joel"/>
   </React.StrictMode>,
   document.getElementById('root')
 );
